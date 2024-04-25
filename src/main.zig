d1d905a92e1702ea8f16e34d73d6b707e67f67e1
Felix Caraballo <caraballofelix88@gmail.com>
2024-04-25 11:13:11 -0400

    hell yea listing_0039 works, feeling good

--- src/main.zig
+++ src/main.zig
@@ -2,25 +2,62 @@ const std = @import("std");
 
 pub fn main() !void {
     var file_buf: [1024]u8 = undefined;
-    const data = try std.fs.cwd().readFile("../computer_enhance/perfaware/part1/listing_0037_single_register_mov", &file_buf);
-    const out_writer = std.io.getStdOut().writer();
+    const listing = "listing_0039_more_movs";
+    const data = try std.fs.cwd().readFile("../computer_enhance/perfaware/part1/" ++ listing, &file_buf);
 
-    parse(out_writer, data);
+    const out_file = try std.fs.cwd().createFile(listing ++ "_ours.asm", .{});
+
+    try parse(out_file.writer(), data);
 }
 
-fn parse(writer: anytype, data: []const u8) !void {
+fn parse(writer: anytype, data: []const u8, args: Args) !void {
     var idx: usize = 0;
+
     while (idx < data.len) {
         const inst_slice = Instruction.getNext(data[idx..]);
+
+        if (args.print) {
+            std.debug.print("getOp bin: {b}\n", .{inst_slice});
+            std.debug.print("parsed op: {s}\n", .{@tagName(getOp(inst_slice).?)});
+            std.debug.print("getLen: {}\n", .{inst_slice.len});
+        }
 
         const opcode = Instruction.getOpcode(getOp(inst_slice).?);
         const operands = Instruction.getOperands(inst_slice);
 
         // TODO: handle printing operand
+        var op_a_str: [128]u8 = undefined;
+        var op_b_str: [128]u8 = undefined;
+
+        var rendered_a: []u8 = undefined;
+        var rendered_b: []u8 = undefined;
+
+        switch (operands[0]) {
+            .addr => |addr| {
+                rendered_a = @constCast(try addr.render(&op_a_str));
+            },
+            .imm => |val| {
+                rendered_a = op_a_str[0..std.fmt.formatIntBuf(&op_a_str, val, 10, .lower, .{})];
+            },
+        }
+
+        switch (operands[1]) {
+            .addr => |addr| {
+                rendered_b = @constCast(try addr.render(&op_b_str));
+            },
+            .imm => |val| {
+                rendered_b = op_b_str[0..std.fmt.formatIntBuf(&op_b_str, val, 10, .lower, .{})];
+            },
+        }
+
+        if (args.print) {
+            std.debug.print("parsed:\t{s} {s}, {s}\n\n", .{ @tagName(opcode), rendered_a, rendered_b });
+        }
+
         _ = try writer.print("{s} {s}, {s}\n", .{
             @tagName(opcode),
-            @tagName(operands[0].addr.reg.?),
-            @tagName(operands[1].addr.reg.?),
+            rendered_a,
+            rendered_b,
         });
 
         idx += inst_slice.len;
@@ -37,14 +74,19 @@ const OpType = enum {
     segreg_to_reg,
 };
 
+const REGMEM_TO_REG_OP = 0b10001000;
+const IMM_TO_REGMEM_OP = 0b11000110;
+const IMM_TO_REG_OP = 0b10110000;
+const MEM_TO_ACC_OP = 0b10100000;
+const SEGREG_TO_REG_OP = 0b10001100;
+
 fn getOp(data: []const u8) ?OpType {
     const op = data[0];
 
-    const REGMEM_TO_REG_OP = 0b10001000;
-    const IMM_TO_REGMEM_OP = 0b11000110;
-    const IMM_TO_REG_OP = 0b11010000;
-    const MEM_TO_ACC_OP = 0b10100000;
-    const SEGREG_TO_REG_OP = 0b10001100;
+    // NOTE: masking an imm_to_reg op can match the mask for regmem_to_reg, so we check for imm_to_reg first
+    if (op & IMM_TO_REG_OP == IMM_TO_REG_OP) {
+        return .imm_to_reg;
+    }
 
     if (op & REGMEM_TO_REG_OP == REGMEM_TO_REG_OP) {
         return .regmem_to_reg;
@@ -52,10 +94,6 @@ fn getOp(data: []const u8) ?OpType {
 
     if (op & IMM_TO_REGMEM_OP == IMM_TO_REGMEM_OP) {
         return .imm_to_regmem;
-    }
-
-    if (op & IMM_TO_REG_OP == IMM_TO_REG_OP) {
-        return .imm_to_reg;
     }
 
     if (op & MEM_TO_ACC_OP == MEM_TO_ACC_OP) {
@@ -80,7 +118,8 @@ fn getLength(data: []const u8) u8 {
     const mov_type = getOp(data);
 
     const mod: u2 = @truncate(data[1] >> 6);
-    const wide: bool = @as(u1, @truncate(data[0])) == 1;
+    // lord forgive me
+    const wide: bool = (if (mov_type.? == .imm_to_reg) data[0] >> 3 else data[0]) & 0b1 > 0;
 
     return switch (mov_type.?) {
         .regmem_to_reg => switch (mod) {
@@ -89,14 +128,47 @@ fn getLength(data: []const u8) u8 {
             0b01 => 3,
             0b10 => 4,
         },
-        .imm_to_regmem => if (wide) 3 else 2,
-        .mem_to_acc => 2,
+        .imm_to_regmem => switch (mod) {
+            0b11 => if (wide) 4 else 3,
+            0b00 => if (wide) 4 else 3,
+            0b01 => if (wide) 5 else 4,
+            0b10 => if (wide) 6 else 5,
+        },
+        .mem_to_acc => 3,
         .segreg_to_reg => 4,
+        .imm_to_reg => if (wide) 3 else 2,
         else => unreachable, // TODO: unreachables should be errors.
     };
 }
 
-// TODO: do we really need an object for this? revise later
+test "getLength" {
+    // regmem to reg, no disp
+    try std.testing.expectEqual(2, getLength(&.{ 0b10001001, 0b11011110 }));
+    // regmem to reg, 8-bit disp
+    try std.testing.expectEqual(3, getLength(&.{ 0b10001001, 0b01011110 }));
+    // regmem to reg, 16-bit disp
+    try std.testing.expectEqual(4, getLength(&.{ 0b10001001, 0b10011110 }));
+
+    // imm to regmem, no disp, 8-bit data
+    try std.testing.expectEqual(3, getLength(&.{ 0b11000110, 0b11011110 }));
+    // imm to regmem, no disp, 16-bit data
+    try std.testing.expectEqual(4, getLength(&.{ 0b11000111, 0b11011110 }));
+
+    // imm to regmem, 8-bit disp, 8-bit data,
+    try std.testing.expectEqual(4, getLength(&.{ 0b11000110, 0b01011110 }));
+    // imm to regmem, 16-bit disp, 8-bit data
+    try std.testing.expectEqual(5, getLength(&.{ 0b11000110, 0b10011110 }));
+
+    // imm to regmem, 8-bit disp, 16-bit data,
+    try std.testing.expectEqual(5, getLength(&.{ 0b11000111, 0b01011110 }));
+    // imm to regmem, 16-bit disp, 16-bit data
+    try std.testing.expectEqual(6, getLength(&.{ 0b11000111, 0b10011110 }));
+
+    // imm to reg
+    try std.testing.expectEqual(2, getLength(&.{ 0b10110000, 0b10011110 }));
+    try std.testing.expectEqual(3, getLength(&.{ 0b10111000, 0b10011110 }));
+}
+
 const Instruction = struct {
 
     // returns slice representing next valid instruction.
@@ -147,7 +219,7 @@ const Instruction = struct {
         const op = getOp(inst_data);
         var operands: [2]Operand = undefined;
 
-        if (op == .regmem_to_reg) {
+        if (op.? == .regmem_to_reg) {
             const mod: u2 = @truncate(inst_data[1] >> 6);
             const wide: bool = boolFromBit(inst_data[0]);
 
@@ -157,14 +229,14 @@ const Instruction = struct {
             // usually starts at 3rd byte if its there
             const disp: u16 = getWide(inst_data, 2);
 
-            const a = getAddress(mod, reg, wide, disp);
+            const a = getAddress(0b11, reg, wide, disp);
             const b = getAddress(mod, rm, wide, disp);
 
             operands = .{
                 .{ .addr = a },
                 .{ .addr = b },
             };
-        } else if (op == .imm_to_regmem) {
+        } else if (op.? == .imm_to_regmem) {
             const mod: u2 = @truncate(inst_data[1] >> 6);
             const wide: bool = boolFromBit(inst_data[0]);
             const rm: u3 = @truncate(inst_data[1]);
@@ -175,10 +247,20 @@ const Instruction = struct {
             const addr: Address = getAddress(mod, rm, wide, disp);
 
             operands = .{ .{ .addr = addr }, .{ .imm = data } };
+        } else if (op.? == .imm_to_reg) {
+            const wide: bool = boolFromBit(inst_data[0] >> 3);
+            const reg: u3 = @truncate(inst_data[0]);
+
+            const data: u16 = if (wide) getWide(inst_data, 1) else inst_data[1];
+
+            operands = .{
+                .{ .addr = getAddress(0b11, reg, wide, 0) },
+                .{ .imm = data },
+            };
         }
 
         // TODO: this doesnt hold for certain ops.
-        if (!getDestinationBit(inst_data)) {
+        if (op.? != .imm_to_reg and !getDestinationBit(inst_data)) {
             const swap = operands[0];
             operands[0] = operands[1];
             operands[1] = swap;
@@ -191,7 +273,7 @@ const Instruction = struct {
     fn getAddress(mod: u2, reg: u3, wide: bool, disp: u16) Address {
         var address: Address = .{};
 
-        // handle direct address
+        // TODO: handle direct address
         if (mod == 0b00 and reg == 0b110) {
             address.offset = 0;
             return address; // lol i dont know what goes here actually
@@ -204,8 +286,12 @@ const Instruction = struct {
         }
 
         // handle regmem, wow this is yucky
+        address.indirect = true;
+        // TODO: going through each reg value in a big switch case would be clearer
         if (reg == 0b111) {
             address.reg = .bx;
+        } else if (reg == 0b110) {
+            address.reg = .bp;
         } else {
             if (reg & 0b001 > 0) {
                 address.memreg = .di;
@@ -233,8 +319,6 @@ const Instruction = struct {
 
         return address;
     }
-
-    fn getMemReg() Address {}
 
     fn getRegister(reg: u3, wide: bool) Register {
         const high: bool = reg & 0b100 > 0;
@@ -273,9 +357,42 @@ const Instruction = struct {
 };
 
 const Address = struct {
-    reg: ?Register = null, // no register == immediate addresso
+    reg: ?Register = null, // no register == immediate address
     memreg: ?Register = null, // for SI, DI. Probably not the right name
     offset: u16 = 0, // DISP
+    indirect: bool = false, // indicates the address is to be derived from here
+
+    pub fn render(a: Address, buf: []u8) ![]const u8 {
+        var local_buf: [256]u8 = undefined;
+        var fba = std.heap.FixedBufferAllocator.init(&local_buf);
+        const alloc = fba.allocator();
+
+        var list = std.ArrayList([]const u8).init(alloc);
+        defer list.deinit();
+
+        // max rendered val will be 65536
+        var offset_buf: [5]u8 = undefined;
+        _ = std.fmt.formatIntBuf(&offset_buf, a.offset, 10, .lower, .{});
+        const offset_slice = std.mem.trim(u8, &offset_buf, "\xaa");
+
+        const elems = [_][]const u8{
+            if (a.reg != null) @tagName(a.reg.?) else "",
+            if (a.memreg != null) @tagName(a.memreg.?) else "",
+            if (a.offset > 0) offset_slice else "",
+        };
+
+        for (elems) |item| {
+            if (item.len > 0) {
+                try list.append(item);
+            }
+        }
+
+        const elems_str = try std.mem.join(alloc, " + ", list.items);
+
+        const maybe_left_br = if (a.indirect) "[" else "";
+        const maybe_right_br = if (a.indirect) "]" else "";
+        return try std.fmt.bufPrint(buf, "{s}{s}{s}", .{ maybe_left_br, elems_str, maybe_right_br });
+    }
 };
 
 const Operand = union(enum) { addr: Address, imm: u16 };
@@ -325,10 +442,10 @@ fn compareToNasm(alloc: std.mem.Allocator, comptime listing: []const u8, args: A
     try parse(output_stream.writer(), binary_data);
 
     var output_file = try std.fs.cwd().createFile(listing ++ "_ours" ++ ".asm", .{});
+    defer std.fs.cwd().deleteFile(listing ++ "_ours" ++ ".asm") catch {};
+
     std.debug.print("Output ASM:\n{s}\n", .{output_stream.getWritten()});
     _ = try output_file.writeAll(output_stream.getWritten());
-
-    defer std.fs.cwd().deleteFile(listing ++ "_ours" ++ ".asm") catch {};
 
     // assemble our file
     const our_nasm_args = [_][]const u8{ "nasm", listing ++ "_ours" ++ ".asm", "-o", listing ++ "_ours" };
